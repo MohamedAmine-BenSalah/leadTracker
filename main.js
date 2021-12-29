53e0327c2b2a1d1e42f53b132dac5430139e3931
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2021-12-29 18:36:57 +0100

    renderLeads

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 let ulEl = document.getElementById('ul-el');
@@ -6,10 +6,18 @@ let ulEl = document.getElementById('ul-el');
 
 inputBtn.addEventListener('click',()=> {
     let inputLead = inputEl.value;
+    
     myLeads.push(inputLead);
+    renderLeads();  
+})
+let renderLeads = () => {
+    
+    let listItems = "";
     for (let i=0;i<myLeads.length;i++) {
-        ulEl.innerHTML +=  "<li>" +  myLeads[i] + "</li> "
+    listItems +=  "<li>" +  myLeads[i] + "</li> "
     }
     
+    //manipulating the DOM is expensive so it's better to do it out side of the for loop
+    ulEl.innerHTML = listItems
     console.log(myLeads);
-})
+}
